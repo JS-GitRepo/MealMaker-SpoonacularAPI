e1467c539b33ca009965222e1440461234aa2109
Isaiah <isaiah.sweezie@gmail.com>
2022-03-17 14:01:52 -0400

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,16 @@ import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
-
-  return <div className="Header">
-    <Link to={`/favorites`}><i className="fa-brands fa-gratipay"></i></Link>
-  </div>;
+  return (
+    <header className="Header">
+      <i className="burgerIcon"></i>
+      <h1>MealMaker</h1>
+      <p className="header-slogan">meals from your pantry...</p>
+      <Link to={`/favorites`}>
+        <i className="fa-brands fa-gratipay"></i>
+      </Link>
+    </header>
+  );
 };
 
 export default Header;
